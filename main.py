8e4213f68fdc74112ea0f523317128ac0abbf2d5
Александр Семененко <stud0000264339@study.utmn.ru>
2025-11-05 22:28:51 +0500

    fix flake8

--- main.py
+++ main.py
@@ -7,10 +7,14 @@ st.image("960.jpg")
 st.title("Количество пассажиров каждого пола по классу обслуживания")
 class_selected = st.selectbox("Выберите класс обслуживания", (1, 2, 3))
 
+
 def get_gender_counts(df, class_selected):
-    men_count = df[(df["Pclass"] == class_selected) & (df["Sex"] == "male")].shape[0]
-    women_count = df[(df["Pclass"] == class_selected) & (df["Sex"] == "female")].shape[0]
+    men_count = df[
+        (df["Pclass"] == class_selected) & (df["Sex"] == "male")].shape[0]
+    women_count = df[
+        (df["Pclass"] == class_selected) & (df["Sex"] == "female")].shape[0]
     return men_count, women_count
+
 
 men_count, women_count = get_gender_counts(df, class_selected)
 table = {
